2a0208ee4157fc5d612b0ca73be177a4d576ddb9
koomekevo <koome.kevo@gurucrafts.net>
2023-01-09 19:47:37 +0300

    Run pip freeze > requirements.txt

--- myproject/settings.py
+++ myproject/settings.py
@@ -27,8 +27,9 @@ SECRET_KEY = 'aay0j_9b&ky3a7(8m8il+-1ud(scw12@w5!+5-=gsk6ynzi0ls'
 DEBUG = True
 
 # <HINT> add your cloud host here
-ALLOWED_HOSTS = ['*']
-CSRF_TRUSTED_ORIGINS = ["https://*.fly.dev"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "django-online-course.fly.dev"]
+
+CSRF_TRUSTED_ORIGINS = ["https://django-online-course.fly.dev"]
 
 
 # Application definition
