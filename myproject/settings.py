abb3065fd6e4f4a8d06364a190837982842534dc
koomekevo <koome.kevo@gurucrafts.net>
2023-01-12 14:03:12 +0300

    Change from Django 3.1.3 to Django 4.1.5

--- myproject/settings.py
+++ myproject/settings.py
@@ -12,10 +12,13 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import dj_database_url
+from dotenv import load_dotenv
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+load_dotenv(BASE_DIR / '.env')
 
 
 # Quick-start development settings - unsuitable for production
